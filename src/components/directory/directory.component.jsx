3262479ec70a55bfd4fe372d7417e4cde12bb345
Deedee <dorsnet@hotmail.com>
2021-06-21 11:08:03 +0200

    Initial commit, added redux to project

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import MenuItem from '../menu-item/menu-item.component';
+import './directory.styles.scss';
+
+class Directory extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections: [
+        {
+          title: 'hats',
+          imageUrl:
+            'https://images.unsplash.com/photo-1552060155-4eac706a5515?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1189&q=80',
+          id: 1,
+          linkUrl: 'hats',
+        },
+        {
+          title: 'jackets',
+          imageUrl:
+            'https://images.unsplash.com/photo-1601924994987-69e26d50dc26?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+          id: 2,
+          linkUrl: '',
+        },
+        {
+          title: 'sneakers',
+          imageUrl:
+            'https://images.unsplash.com/photo-1495555961986-6d4c1ecb7be3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+          id: 3,
+          linkUrl: '',
+        },
+        {
+          title: 'humans',
+          imageUrl:
+            'https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80',
+          size: 'large',
+          id: 4,
+          linkUrl: '',
+        },
+        {
+          title: 'humans',
+          imageUrl:
+            'https://images.unsplash.com/photo-1530031092055-18d4a16ff6e5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80',
+          size: 'large',
+          id: 5,
+          linkUrl: '',
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Directory;
